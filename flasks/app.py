be01350db75ff89bf46a8d2c332cf47a6811d54d
Gilson <115327502+gil779023@users.noreply.github.com>
2023-10-15 11:14:38 -0300

    atualizado

--- flasks/app.py
+++ flasks/app.py
@@ -4,12 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello World</h1>"
+    return """<h1>Hello World</h1>\
+        
+    <a href="/home">Ver mais</a><br/>"""
 
 @app.route("/home")
 def home_page():
     return """<h2>homepage</h2>
-<img width="150px"  src=""/>
+<img width="150px"  src="https://th.bing.com/th/id/OIP.Xj_XB9KGp8Oon6kxGo69OgHaE-?pid=ImgDet&rs=1"/>
 <a href="/">Ver mais</a><br/>
 """
 
